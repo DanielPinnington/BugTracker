6055f8d708e02837d42068c128a770a01597c66f
DanielPinnington <danielpinnington12@gmail.com>
2021-05-25 14:09:57 +0000

    Testing login page, saving my progress.
    Testing log in page. not currently working. Basic sign up form implemented, eventually will implement an email sign up confirmation email.

--- BugTracker/Controllers/HomeController.cs
+++ BugTracker/Controllers/HomeController.cs
@@ -1,15 +1,23 @@
 ﻿using BugTracker.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using static DataLibrary.Logic.EmployeeProcessor;
-
+using static DataLibrary.DataAccess.SqlDataAccess;
+using System.Configuration;
+using DataLibrary.DataAccess;
+using System.Data.SqlClient;
+using Google.Apis.Admin.Directory.directory_v1.Data;
+using BugTracker.Models;
 namespace BugTracker.Controllers
 {
     public class HomeController : Controller
     {
+        
         public ActionResult Index()
         {
             return View();
@@ -28,9 +36,40 @@ namespace BugTracker.Controllers
 
             return View();
         }
+        public ActionResult ViewEmployees()
+        {
+            ViewBag.Message = "Employees List";
+
+            var data = LoadEmployees();
+            List<EmployeeModel> employees = new List<EmployeeModel>();
+
+            foreach(var row in data)
+            {
+                employees.Add(new EmployeeModel{
+                    EmployeeID = row.EmployeeID,
+                    UsernameID = row.UsernameID,
+                    FirstName = row.FirstName,
+                    SecondName = row.SecondName,
+                    EmailAddress = row.EmailAddress
+                    
+                });
+            }
+
+            return View(employees);
+        }
+
         public ActionResult SignUp()
         {
             ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult LogIn()
+        {
+            string debugDBConnection = ConfigurationManager.ConnectionStrings["BugTrackerDB"].ConnectionString;
+            SqlConnection sqlConn = new SqlConnection(debugDBConnection);
+            SqlCommand sqlComm = new SqlCommand("select * from [db].[employee] where Username = @Username and Password = @Password", sqlConn);
 
             return View();
         }
@@ -38,6 +77,7 @@ namespace BugTracker.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult SignUp(EmployeeModel model)
         {
+
             if (ModelState.IsValid)
             {
                 int recordsCreated = CreateEmployee(model.EmployeeID, model.UsernameID, model.FirstName, model.SecondName, model.EmailAddress);
