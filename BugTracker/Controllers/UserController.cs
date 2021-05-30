d4c31c4509a586b0fac918bfcce23ff1dd6503c9
DanielPinnington <danielpinnington12@gmail.com>
2021-05-30 21:24:37 +0000

    Refactoring code (full rework)
    Refactoring code, full rework to the code. Keeping the SQL database within the project itself, this makes it easier to commit and test things.
    
    Bug - I don't think the project is checking if an email already exists or not yet.

---
+++ BugTracker/Controllers/UserController.cs
@@ -0,0 +1,58 @@
+﻿using BugTracker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BugTracker.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Registration()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Registration([Bind(Exclude ="IsEmailVerified, ActivationCode")] User user)
+        {
+            bool Status = false;
+            string message = "";
+
+            if (ModelState.IsValid)
+            {
+                //Email already exists
+                var doesExist = DoesEmailExist(user.EmailID);
+                if (doesExist)
+                {
+                    ModelState.AddModelError("EmailExists", "Email already exists");
+                    return View(user);
+                }
+            }
+            else
+            {
+                message = "Invalid Request";
+            }
+            return View();
+        }
+
+        [NonAction]
+        public bool DoesEmailExist(string emailID)
+        {
+            using(BugTrackerDBEntities dc = new BugTrackerDBEntities())
+            {
+                var v = dc.Users.Where(a => a.EmailID == emailID).FirstOrDefault();
+                return v != null;
+            }
+        }
+    }
+}
---
+++ BugTracker/Models/Extended/User.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+
+namespace BugTracker.Models
+{
+    [MetadataType(typeof(UserMetadata))]
+    public partial class User
+    {
+        public string ConfirmPassword { get; set; }
+    }
+
+   
+    public class UserMetadata
+    {
+        [Display(Name = "First Name")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "First name required")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        [Required, MaxLength(16)] //Max length of username is 16
+        public String LastName { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email ID")]
+        [Required, MaxLength(30)] //Max length of username is 30
+        public String EmailID { get; set; }
+
+        [Display(Name = "Date of Birth")]
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy}")]
+        public DateTime DateOfBirth { get; set; }
+
+        [DataType(DataType.Password)]
+        [StringLength(20)] //Can add MinimumLength StringLength(20), MinimumLength(4)
+        public String Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Password is not the same")]
+        public String ConfirmPassword { get; set; }
+
+    }
+}
