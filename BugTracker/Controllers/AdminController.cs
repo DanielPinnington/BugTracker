e4a8643cf4da09564e2cc27244a2964b1e28a15b
DanielPinnington <danielpinnington12@gmail.com>
2021-06-10 18:46:19 +0000

    Setting up Admin Panel DB/Views/Controllers
    Testing Admin permissions, slightly tricky as not much information on it so I'm going to keep it simple and implement an admin as a user to start with.

---
+++ BugTracker/Controllers/AdminController.cs
@@ -0,0 +1,226 @@
+﻿using BugTracker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Runtime.InteropServices;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace BugTracker.Controllers
+{
+    public class AdminController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AdminRegistration()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Registration([Bind(Exclude = "IsEmailVerified, ActivationCode")] User user)
+        {
+            bool Status = false;
+            string message = "";
+
+            if (ModelState.IsValid)
+            {
+                //Email already exists
+                var doesExist = DoesEmailExist(user.EmailID);
+                if (doesExist)
+                {
+                    ModelState.AddModelError("EmailExists", "Email already exists");
+                    return View(user);
+                }
+
+                #region Generate Activation Code
+                user.ActivationCode = Guid.NewGuid();
+                #endregion
+
+                #region Password Hashing
+                user.Password = Crypto.Hash(user.Password);
+                user.ConfirmPassword = Crypto.Hash(user.ConfirmPassword);
+                #endregion
+                user.IsEmailVerified = false;
+
+
+                #region Save to Database
+                using (BugTrackerDBEntities dc = new BugTrackerDBEntities())
+                {
+                    dc.Users.Add(user);
+                    dc.Configuration.ValidateOnSaveEnabled = false;
+                    dc.SaveChanges();
+
+                    SendVerificationLinkEmail(user.EmailID, user.ActivationCode.ToString());
+                    message = "Registration successfully done. Account activation link has been sent to your email ID:" + user.EmailID;
+                    Status = true;
+
+                }
+
+                #endregion
+            }
+            else
+            {
+                message = "Invalid Request";
+            }
+            ViewBag.Message = message;
+            ViewBag.Status = Status;
+            return View();
+        }
+
+
+        [HttpGet]
+        public ActionResult VerifyAccount(string id)//activation code
+        {
+            bool Status = false;
+
+            using (BugTrackerDBEntities dc = new BugTrackerDBEntities())
+            {
+                dc.Configuration.ValidateOnSaveEnabled = false; //This is to avoid confirm password (does not match) issue
+                var v = dc.Users.Where(a => a.ActivationCode == new Guid(id)).FirstOrDefault();
+                if (v != null)
+                {
+                    v.IsEmailVerified = true;
+                    dc.SaveChanges();
+                    Status = true;
+                }
+                else
+                {
+                    ViewBag.Message = "Invalid Request";
+                }
+            }
+            ViewBag.Status = true;
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(UserLogin login, string ReturnURL = "")
+        {
+            string message = "";
+            using (BugTrackerDBEntities dc = new BugTrackerDBEntities())
+            {
+                var v = dc.Users.Where(a => a.EmailID == login.emailID).FirstOrDefault();
+                if (v != null)
+                {
+                    if (string.Compare(Crypto.Hash(login.Password), v.Password) == 0)
+                    {
+                        int timeout = login.RememberMe ? 525600 : 20; //If remember me, remember this for one year! (525600)
+                        var ticket = new FormsAuthenticationTicket(login.emailID, login.RememberMe, timeout);
+                        string encrypted = FormsAuthentication.Encrypt(ticket);
+                        var cookie = new HttpCookie(FormsAuthentication.FormsCookieName, encrypted);
+                        cookie.Expires = DateTime.Now.AddMinutes(timeout); //ONE YEAR (TIMEOUT)
+                        cookie.HttpOnly = true;
+
+                        Response.Cookies.Add(cookie);
+
+                        if (Url.IsLocalUrl(ReturnURL))
+                        {
+                            return Redirect(ReturnURL);
+                        }
+                        else
+                        {
+                            return RedirectToAction("Index", "Home");
+                        }
+                    }
+                }
+                else
+                {
+                    message = "Invalid Credentials";
+                }
+            }
+            ViewBag.Message = "";
+
+            return View();
+        }
+
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult Logout()
+        {
+
+            FormsAuthentication.SignOut();
+
+
+            return RedirectToAction("Login", "User");
+        }
+
+
+
+        [NonAction]
+        public bool DoesEmailExist(string emailID)
+        {
+            using (BugTrackerDBEntities dc = new BugTrackerDBEntities())
+            {
+                var v = dc.Users.Where(a => a.EmailID == emailID).FirstOrDefault();
+                return v != null;
+            }
+        }
+
+        [NonAction]
+        public void SendVerificationLinkEmail(string emailID, string activationCode, string emailFor = "VerifyAccount")
+        {
+            var scheme = Request.Url.Scheme;
+            var host = Request.Url.Host;
+            var port = Request.Url.Port;
+
+            var verifyURL = "/User/" + emailFor + "/" + activationCode;
+            var link = Request.Url.AbsoluteUri.Replace(Request.Url.PathAndQuery, verifyURL);
+
+            var fromEmail = new MailAddress("BugTrackerTest12@gmail.com");
+            var toEmail = new MailAddress(emailID);
+            var fromEmailPassword = "cazhdkrjmgedcqll";
+
+            string subject = "";
+            string body = "";
+            if (emailFor == "VerifyAccount")
+            {
+
+                subject = "Your account is successfully created!";
+                body = "<br/><br/> We are excited to tell you that your BugTracker account is " +
+                                " successfully created! Please click on the below link to verify your account" +
+                                "<br/><br/><a href='" + link + "'>" + link + "</a> ";
+
+            }
+            else if (emailFor == "ResetPassword")
+            {
+                subject = "Reset Password";
+                body = "Hi, <br/>br/>We got a request to reset your account, please click on the below link to reset your password." +
+                    "<br/><br/><a href='" + link + ">Reset Password Link</a> ";
+            }
+            var smtp = new SmtpClient
+            {
+                EnableSsl = true,
+                Host = "smtp.gmail.com",
+                Port = 587,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(fromEmail.Address, fromEmailPassword)
+            };
+
+            using (var message = new MailMessage(fromEmail, toEmail)
+            {
+                Subject = subject,
+                Body = body,
+                IsBodyHtml = true
+            })
+                smtp.Send(message);
+        }
+
+    }
+}
---
+++ BugTracker/Models/Extended/AdUser.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+
+namespace BugTracker.Models
+{
+    [MetadataType(typeof(UserMetadataAdmin))]
+   // public partial class Admin
+   // {
+   //     public string ConfirmPassword { get; set; }
+   // }
+
+   
+    public class UserMetadataAdmin
+    {
+
+
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email ID")]
+        [Required, MaxLength(30)] //Max length of username is 30
+        public String EmailID { get; set; }
+
+        [Display(Name = "Date of Birth")]
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy}")]
+        public DateTime DateOfBirth { get; set; }
+
+        [DataType(DataType.Password)]
+        [StringLength(20)] //Can add MinimumLength StringLength(20), MinimumLength(4)
+        public String Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Password is not the same")]
+        public String ConfirmPassword { get; set; }
+
+    }
+}
