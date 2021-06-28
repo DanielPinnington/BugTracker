db1a941802761cafa9a5f7abf7928427a57618c4
DanielPinnington <danielpinnington12@gmail.com>
2021-06-28 18:15:07 +0000

    View/UI/Design changes
    Tweaking UI/Views as I'm still testing styles in bootstrap/css

--- BugTracker/Models/UserLogin.cs
+++ BugTracker/Models/UserLogin.cs
@@ -8,11 +8,11 @@ namespace BugTracker.Models
 {
     public class UserLogin
     {  
-        [Display(Name = "Email ID")]
-        [Required(AllowEmptyStrings = false, ErrorMessage = "Email ID Required")]
+        [Display(Name = "Username")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Username Required")]
         public string emailID { get; set; }
 
-        [Required(AllowEmptyStrings = false, ErrorMessage = "Email ID Required")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Password Required")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
