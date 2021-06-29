0be0c973912c6f90183f78c290ab0743a4c947fe
DanielPinnington <danielpinnington12@gmail.com>
2021-06-29 22:41:54 +0000

    Implemented search functionality
    Half way done with search implementation, will need to update the "sorting" at the moment, it is searching correctly by username and title but I will need to figure out how to sort/filter by priority.
    
    Also (BUG) search functionality for strings is crashing, no idea why but I will look into this tomorrow. I have provided a hyperlink to the bug within the bugtracking system.

--- BugTracker/App_Start/RouteConfig.cs
+++ BugTracker/App_Start/RouteConfig.cs
@@ -15,8 +15,9 @@ namespace BugTracker
 
             routes.MapRoute(
                 name: "Default",
-                url: "{controller}/{action}/{id}",
+                url: "{controller}/{action}", //{id}
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                //{id}
             );
         }
     }
--- BugTracker/Controllers/TicketController.cs
+++ BugTracker/Controllers/TicketController.cs
@@ -1,17 +1,52 @@
-﻿using System;
+﻿using BugTracker.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Linq.Dynamic;
 using System.Web.Mvc;
+using System.Web.UI;
+using System.Xml.Schema;
 
 namespace BugTracker.Controllers
 {
     public class TicketController : Controller
     {
+        
         // GET: Ticket
-        public ActionResult Index()
+        public ActionResult GetTicketsIndex(int page = 1, string sort = "Title", string sortdir = "asc", string search = "")
         {
-            return View();
+            int totalRecord =0;
+            int pageSize = 10;
+            if (page < 1) page = 1;
+            int skip = (page * pageSize) - pageSize;
+            var data = GetTickets(search, sort, sortdir, skip, pageSize, out totalRecord);
+            ViewBag.TotalRows = totalRecord;
+            ViewBag.search = search;
+            return View(data);
+        }
+        public List<Ticket> GetTickets(string search, string sort, string sortdir, int skip, int pageSize, out int totalRecord)
+            {
+                using(BugTracking dc = new BugTracking())
+                {
+                    var v = (from a in dc.Tickets
+                             where
+                             a.Title.Contains(search) ||
+                             a.Description.Contains(search) ||
+                             //date should be here will add later
+                             a.CurrentUser.Contains(search) ||
+                             a.Priorities.Contains(search)
+
+                             select a
+                             );
+                totalRecord = v.Count();
+                v = v.OrderBy(sort + " " + sortdir);
+                if (pageSize > 0)
+                {
+                    v = v.Skip(skip).Take(pageSize);
+                }
+                return v.ToList();
+            }
         }
     }
 }
