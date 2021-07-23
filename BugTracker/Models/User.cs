8a1f8b94456d8178f87deae00b22564730b276f3
DanielPinnington <danielpinnington12@gmail.com>
2021-07-23 14:18:18 +0000

    Small DB rename + Layout Authentication
    DB Tables renamed althroughout the project.
    
    Layout page authentication, when a user is NOT authenticated display register/login buttons. When authenticated remove register/login.

--- BugTracker/Models/User.cs
+++ BugTracker/Models/User.cs
@@ -15,7 +15,6 @@ namespace BugTracker.Models
     public partial class User
     {
         public int Id { get; set; }
-        
         public string Username { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
